9205ae47f7d520d8e778c80f8c1e6afba99ad4b0
Kwadjo Ampadu <josephampadu549@gmail.com>
2020-10-27 15:42:03 +0000

    seo stuff

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { Code, Text } from "@chakra-ui/core";
+import Head from "next/head";
 
 import { Container } from "../components/Container";
 import { CTA } from "../components/CTA";
@@ -9,6 +10,27 @@ import { Main } from "../components/Main";
 
 const Index = () => (
   <Container height="100vh">
+    <Head>
+      <title>Kwadjo Ampadu</title>
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      {/* <link rel="manifest" href="/site.webmanifest" /> */}
+    </Head>
     <Hero />
     <Main>
       <Text textAlign="center">
