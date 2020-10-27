33eca0808c23cfd7da9d4e1a53fd17babee437dc
Kwadjo Ampadu <josephampadu549@gmail.com>
2020-10-27 10:12:54 +0000

    hacky

---
+++ src/components/CTA.tsx
@@ -0,0 +1,53 @@
+import { Button, Link as ChakraLink } from "@chakra-ui/core";
+import { SiGithub, SiTwitter } from "react-icons/si";
+
+import { Container } from "./Container";
+
+export const CTA = () => (
+  <Container
+    flexDirection="row"
+    position="fixed"
+    bottom="0"
+    width="100%"
+    maxWidth="48rem"
+    py={2}
+  >
+    <ChakraLink
+      isExternal
+      href="https://github.com/daduam"
+      flexGrow={1}
+      mx={2}
+    >
+      <Button
+        width="100%"
+        colorScheme="gray"
+        variant="solid"
+        leftIcon={<SiGithub />}
+      >
+        Github
+      </Button>
+    </ChakraLink>
+
+    <ChakraLink
+      isExternal
+      href="https://twitter.com/mpadu_"
+      flexGrow={1}
+      mx={2}
+    >
+      <Button width="100%" colorScheme="twitter" leftIcon={<SiTwitter />}>
+        Twitter
+      </Button>
+    </ChakraLink>
+
+    <ChakraLink
+      isExternal
+      href="mailto:josephampadu549@gmail.com"
+      flexGrow={3}
+      mx={2}
+    >
+      <Button width="100%" colorScheme="red">
+        Contact Me
+      </Button>
+    </ChakraLink>
+  </Container>
+);
---
+++ src/pages/index.tsx
@@ -0,0 +1,26 @@
+import { Code, Text } from "@chakra-ui/core";
+
+import { Container } from "../components/Container";
+import { CTA } from "../components/CTA";
+import { DarkModeSwitch } from "../components/DarkModeSwitch";
+import { Footer } from "../components/Footer";
+import { Hero } from "../components/Hero";
+import { Main } from "../components/Main";
+
+const Index = () => (
+  <Container height="100vh">
+    <Hero />
+    <Main>
+      <Text textAlign="center">
+        I like to write JavaScript web apps with <Code>Node.js</Code> and{" "}
+        <Code>React</Code>.
+      </Text>
+    </Main>
+
+    <DarkModeSwitch />
+    <Footer></Footer>
+    <CTA />
+  </Container>
+);
+
+export default Index;
