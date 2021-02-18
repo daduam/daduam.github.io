c5fccab4c3e70671657fc3757e76d4d4d0152acf
Kwadjo Ampadu <josephampadu549@gmail.com>
2021-02-18 13:24:39 +0000

    theme colors

---
+++ src/theme.ts
@@ -0,0 +1,16 @@
+export default {
+  colors: {
+    transparent: "transparent",
+    black: "#000",
+    white: "#fff",
+    main: "#384284",
+    shades: {
+      light: "#e5ebe9",
+      dark: "#000000",
+    },
+    accents: {
+      light: "#68aac8",
+      dark: "#6673a6",
+    },
+  },
+};
