eb1c56af0a1a6fae7173796f7c2deb3ce3ee2e76
Kwadjo Ampadu <josephampadu549@gmail.com>
2021-03-02 20:48:06 +0000

    headline paragraph

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,19 @@
+import { Box } from "@chakra-ui/react";
 import { Layout } from "../components/Layout";
 
 const Index = () => {
-  return <Layout height="100vh">about me</Layout>;
+  return (
+    <Layout height="100vh">
+      <Box w="container.md" p="8">
+        <p>
+          <em>Hello</em>, I am <strong>Joseph Ampadu</strong>, a computer
+          science undergraduate and <strong>fullstack web developer</strong>.
+          I am currently <strong>open</strong> to entry level developer roles
+          and internships.
+        </p>
+      </Box>
+    </Layout>
+  );
 };
 
 export default Index;
