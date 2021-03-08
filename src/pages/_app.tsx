0d6bf81baa65e28291e4cf9ef91b397f37b178ba
Kwadjo Ampadu <josephampadu549@gmail.com>
2021-03-08 23:05:46 +0000

    analytics stuff

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,26 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { AppProps } from "next/app";
+import { useRouter } from "next/dist/client/router";
+import { useEffect } from "react";
 
+import * as gtag from "../lib/gtag";
 import theme from "../theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: URL) => {
+      gtag.pageview(url);
+    };
+
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <ChakraProvider resetCSS theme={theme}>
       <Component {...pageProps} />
